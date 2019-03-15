3578c3a0996863114b829e0e9e970b42f3aa9ac8
nikhilmaram <nike.maram@gmail.com>
2019-03-15 16:34:00 -0700

    split the functionality into different files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include "student_course.h"
+
 
 #include <iostream>
 #include <string>
-
+#include "student_course.h"
+#include "print_funcs.h"
 using namespace std;
 
 bool string_compare(string s1, string s2){
@@ -70,38 +71,6 @@ void add_student_by_name(Course* course_list, int student_id, string first_name,
     }
 }
 
-void print_student(Student *std_ptr)
-{
-	// if pointer is NULL print the student is not present
-	if(std_ptr == NULL)
-	{
-		cout << "No student present." << endl;
-	}	
-
-	cout << "Id: " << std_ptr -> id << ", First Name: " << std_ptr -> first_name << ", End Name: " << std_ptr -> last_name << endl;
-
-}
-void print_course(Course *course_ptr)
-{
-	if(course_ptr == NULL)
-	{
-		cout << "No course is present." << endl;
-	}
-	Student *std_ptr = course_ptr -> head;
-	if(std_ptr != NULL)
-	{
-		cout << "Printing students in the course." << endl;
-	}
-	else
-	{
-		cout << "No students in the course." << endl;
-	}
-	while(std_ptr != NULL)
-	{
-		print_student(std_ptr);
-		std_ptr = std_ptr -> next;
-	}
-}
 
 
 
---
+++ print_funcs.h
@@ -0,0 +1,6 @@
+#ifndef PRINTFUNCS_H 
+#define  PRINTFUNCS_H
+#include "student_course.h"
+void print_student(Student *std_ptr);
+void print_course(Course *course_ptr);
+#endif
