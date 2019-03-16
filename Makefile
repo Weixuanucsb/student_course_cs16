cf18c835f2c9153261aedf4ad2031896423416dc
Rachel Redberg <rredberg@ucsb.edu>
2019-03-16 09:08:42 -0700

    Added tests for remove_student, added to the Makefile and fixed empty list seg fault in remove_student. (Now featuring the correct files...)

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+remove_student_id_tests: remove_student.o remove_student_id_tests.o 
+	g++ $^ -o $@
+
 reverse_course_list_tests: reverse_course_list.o reverse_course_list_tests.o
 	g++ $^ -o $@
 
 edit_student_name_tests: edit_student_name.o edit_student_name_tests.o
+	g++ $^ -o $@
+
+remove_student_id_tests: remove_student.o remove_student_id_tests.o
 	g++ $^ -o $@
 
 clean:
--- remove_student.cpp
+++ remove_student.cpp
@@ -6,6 +6,10 @@
 // Precondition: We are given a (possibly) empty list of students, each student with a unique ID, and a student ID which doesn't necessarily correspond to a student in the list.
 // Postcondition: If the student with the given ID exists in the list, that student will be removed.
 int remove_student_by_id(Course* course_list, int student_id) {
+	// If the list is empty
+	if (course_list -> head == NULL) {
+		return 0;
+	}
 	Student* iter = course_list -> head;
 	// If the student is at the beginning of the list
 	if (iter -> id == student_id) {
@@ -13,6 +17,7 @@ int remove_student_by_id(Course* course_list, int student_id) {
 		delete iter;
 		return 1;
 	}
+	Student* discard;
 	// If the student is in the middle of the list
 	while (iter -> next != NULL) {
 		if (iter -> next -> id == student_id) {
